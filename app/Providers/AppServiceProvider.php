db583e86e12fd5d142efe54e12a463d0b1588d7a
akari27 <s2122019@stu.musashino-u.ac.jp>
2023-10-21 18:27:08 +0900

    migration

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -25,5 +25,7 @@ class AppServiceProvider extends ServiceProvider
     public function boot()
     {
         Paginator::useBootstrap();
+        \URL::forceScheme('https'); //追加
+        $this->app['request']->server->set('HTTPS','on');
     }
 }
--- resources/views/categories/index.blade.php
+++ resources/views/categories/index.blade.php
@@ -4,24 +4,9 @@
         <meta charset="utf-8">
         <title>Blog</title>
         <!-- Fonts -->
-        <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
     </head>
     <body>
-        <h1>チーム開発会へようこそ！</h1>
-        <h2>カテゴリー:{{ $category_name }} の投稿一覧画面</h2>
-        <a href='/'>投稿一覧ページへ戻る</a>
-        <div>
-            @foreach ($posts as $post)
-                <div style='border:solid 1px; margin-bottom: 10px;'>
-                    <p>
-                        タイトル：<a href="/posts/{{ $post->id }}">{{ $post->title }}</a>
-                    </p>
-                    <p>カテゴリー：<a href="/categories/{{ $post->category->id }}">{{ $post->category->name }}</a></p>
-                </div>
-            @endforeach
-        </div>
-        <div>
-            {{ $posts->links() }}
-        </div>
+        <h1>ハロウィンイベントマッピング</h1>
+        <p>ログインユーザ名 : {{ Auth::user()->name }}</p>
     </body>
 </html>
